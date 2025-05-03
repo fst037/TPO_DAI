2ca24333ab5d5fdafc1d2f05b74314c994cbbcf4
Santiago Carle <Fastest037@users.noreply.github.com>
2025-05-03 16:27:27 -0300

    documentacion de todas los RequestDTO

--- Back End/demo/demo/src/main/java/com/uade/tpo/demo/models/requests/AuthenticationRequest.java
+++ Back End/demo/demo/src/main/java/com/uade/tpo/demo/models/requests/AuthenticationRequest.java
@@ -1,5 +1,6 @@
 package com.uade.tpo.demo.models.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Solicitud para la autenticación de un usuario")
 public class AuthenticationRequest {
+
+  @Schema(description = "Correo electrónico del usuario", example = "usuario@ejemplo.com", required = true)
   String email;
+
+  @Schema(description = "Contraseña del usuario", example = "contraseña123", required = true)
   String password;
 }
--- Back End/demo/demo/src/main/java/com/uade/tpo/demo/models/requests/RatingRequest.java
+++ Back End/demo/demo/src/main/java/com/uade/tpo/demo/models/requests/RatingRequest.java
@@ -1,12 +1,18 @@
 package com.uade.tpo.demo.models.requests;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Solicitud para crear o actualizar una calificación")
 public class RatingRequest {
+
+    @Schema(description = "Calificación otorgada", example = "5", required = true)
     private Integer rating;
+
+    @Schema(description = "Comentarios adicionales sobre la calificación", example = "Excelente servicio", required = true)
     private String comments;
 }
