42846d9adb7a1ab9f694056d73f82e84d398ff58
Christian Salemme <christiancarlossalemme@gmail.com>
2025-06-01 19:49:06 -0300

    Desarrollo TabBar

--- FrontEnd/PocketChef/src/views/Home.jsx
+++ FrontEnd/PocketChef/src/views/Home.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import TabBar from '../components/TabBar';
 
 export default function Home() {
-  useEffect(() => {
+
+  const [active, setActive] = useState(0);
+  
+  /*useEffect(() => {
     // Reemplazá esta URL con la de tu backend real
     fetch('http://localhost:4002/users')
       .then(response => {
@@ -17,12 +21,14 @@ export default function Home() {
       .catch(error => {
         console.error('Error al hacer fetch:', error);
       });
-  }, []);
+  }, []);*/
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <StatusBar barStyle="dark-content" />
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home</Text>
+      </View>
+      <TabBar activeTab={active} onTabPress={setActive} />
     </View>
   );
 }
@@ -32,6 +38,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
 });
