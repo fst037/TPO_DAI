676b1a30facb00a4cca8d053ab95f693275b387b
SantiagoCarle <sacarle@uade.edu.ar>
2025-06-02 15:16:46 -0300

    cambio a axios

--- FrontEnd/PocketChef/src/services/api.js
+++ FrontEnd/PocketChef/src/services/api.js
@@ -1,27 +1,28 @@
+import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const BASE_URL = 'https://orange-broccoli-wr75rxg49pjph5pxj-4002.app.github.dev';
 
 export const NoAuth = async (endpoint, options = {}) => {
-  const res = await fetch(BASE_URL + endpoint, {
-    ...options,
-    headers: {
-      'Content-Type': 'application/json',
-      ...(options.headers || {}),
-    },
-  });
-  return res;
+  const url = BASE_URL + endpoint;
+  const method = options.method ? options.method.toLowerCase() : 'get';
+  const headers = {
+    'Content-Type': 'application/json',
+    ...(options.headers || {}),
+  };
+  const data = options.body ? JSON.parse(options.body) : undefined;
+  return axios({ url, method, headers, data });
 };
 
 export const Auth = async (endpoint, options = {}) => {
   const token = await AsyncStorage.getItem('token');
-  const res = await fetch(BASE_URL + endpoint, {
-    ...options,
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-      ...(options.headers || {}),
-    },
-  });
-  return res;
+  const url = BASE_URL + endpoint;
+  const method = options.method ? options.method.toLowerCase() : 'get';
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+    ...(options.headers || {}),
+  };
+  const data = options.body ? JSON.parse(options.body) : undefined;
+  return axios({ url, method, headers, data });
 };
