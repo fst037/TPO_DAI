df6af8f85aa4d3cdd3b1f78bc436f23c24b808b8
Pilar Jimena Garcia <153676976+pilarty@users.noreply.github.com>
2025-06-01 22:50:30 -0300

    comienzo de receta

--- FrontEnd/PocketChef/App.js
+++ FrontEnd/PocketChef/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import Receta from './src/views/Receta';
 
 export default function App() {
-  return <AppNavigator />;
+  //return <AppNavigator />; //TODO: Descomentar
+  return <Receta />;
 }
