a8edc4bedb3482c79ccbba80663dd7b752f15281
Santiago Carle <Fastest037@users.noreply.github.com>
2025-06-02 00:27:53 -0300

    generic components

---
+++ FrontEnd/PocketChef/src/components/ClickableText.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
+import colors from '../theme/colors';
+
+export default function ClickableText({ children, onPress, style, ...props }) {
+  return (
+    <TouchableOpacity onPress={onPress} {...props}>
+      <Text style={[styles.text, style]}>{children}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: colors.clickableText,
+    textDecorationLine: 'underline',
+    fontSize: 15,
+    marginVertical: 6,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+});
